a967a5a46e56ace8cb5fb93f971e4ee6983f94c1
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2024-11-17 22:59:21 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -10,7 +10,10 @@ from asyncio import (
 from nekozee.filters import command
 from nekozee.handlers import MessageHandler
 from os import execl as osexecl
-from signal import SIGINT, signal
+from signal import (
+    SIGINT,
+    signal
+)
 from sys import executable
 from time import time
 
@@ -41,9 +44,9 @@ from .helper.telegram_helper.bot_commands import BotCommands
 from .helper.telegram_helper.filters import CustomFilters
 from .helper.telegram_helper.message_utils import (
     auto_delete_message,
-    send_message,
     edit_message,
-    send_file
+    send_file,
+    send_message
 )
 from .modules import (
     anonymous,
@@ -141,7 +144,7 @@ async def ping(_, message):
 async def log(_, message):
     await send_file(
         message,
-        "Zee_Logs.txt"
+        "TGH_Logs.txt"
     )
 
 
@@ -298,7 +301,7 @@ async def main():
     if config_dict["DATABASE_URL"]:
         await database.db_load()
     await gather(
-        jdownloader.initiate(),
+        jdownloader.boot(),
         sync_to_async(clean_all),
         bot_settings.initiate_search_tools(),
         restart_notification(),
